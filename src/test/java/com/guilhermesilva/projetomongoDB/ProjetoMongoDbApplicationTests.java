2ab95f2d0475b7cf13ed6431e281112c614b0b2d
welliston <welliston10@gmail.com>
2022-02-21 16:42:10 -0300

    projeto criado

---
+++ src/test/java/com/guilhermesilva/projetomongoDB/ProjetoMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermesilva.projetomongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
