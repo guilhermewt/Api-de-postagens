3f07844334bbcfdd7d2f99d2e0737f72113dc6ae
welliston <welliston10@gmail.com>
2022-02-24 15:36:09 -0300

    insercao de usuario com POST

--- src/main/java/com/guilhermesilva/projetomongoDB/services/UserService.java
+++ src/main/java/com/guilhermesilva/projetomongoDB/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermesilva.projetomongoDB.domain.User;
+import com.guilhermesilva.projetomongoDB.dto.UserDTO;
 import com.guilhermesilva.projetomongoDB.repository.UserRepository;
 import com.guilhermesilva.projetomongoDB.services.exceptions.ObjectNotFound;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFound("object not found"));
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+	
+	public void insert(User obj) {
+		userRepository.save(obj);
 	}
 }
